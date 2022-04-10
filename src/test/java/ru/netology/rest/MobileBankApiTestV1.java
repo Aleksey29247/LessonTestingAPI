f5ecda5776f6367e661cdd6f0bb40a6f8d0d6afb
Aleksey <Alik29247@gmail.com>
2022-04-10 13:59:48 +0700

    Test CI

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -2,11 +2,15 @@ package ru.netology.rest;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static io.restassured.RestAssured.given;
 
 class MobileBankApiTestV1 {
+    Runtime r = Runtime.getRuntime();
     @Test
-    void shouldReturnDemoAccounts() {
+    void shouldReturnDemoAccounts() throws IOException {
+        r.exec("java -jar artifacts/app-mbank.jar");
       // Given - When - Then
       // Предусловия
       given()
